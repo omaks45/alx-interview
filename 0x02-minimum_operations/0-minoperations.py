c3d28a455f1130eca121c12e6ce1388069c610ea
omaks45 <danielgozie5@gmail.com>
2023-05-06 22:50:23 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def minOperations(n):
+  """
+  write a  method that calculates the fewest number of operations
+  needed to result in exactly n H characters in the file.
+  Prototype: def minOperations(n)
+  Returns an integer
+  If n is impossible to achieve, return 0
+  """
+  if n <= 1:
+    return 0
+
+  for e in range(2, (int(n/2)+1)):
+    if n % e == 0:
+      return minOperations(int(n/e)) + e
+  return n
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,9 @@
+Using a recursive method :
+
+n =  the total number of the text in the file.
+x = the smallest prime factor of n.
+y = half the size of n.
+
+We'd say n = x * y.
+if n is a prime number, i.e number divisible by 1 and itself we will have to copy
+the singular H and paste it as n - 1 times giving us n total number of operations
