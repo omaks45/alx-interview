cb11f912c7447fb8df44c3ece5245e943580de66
omaks45 <danielgozie5@gmail.com>
2023-05-07 04:00:37 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,17 +5,16 @@ Solultion to 0.Minimum Operations
 from sys import argv
 
 def minOperations(n):
-  """
-  write a  method that calculates the fewest number of operations
-  needed to result in exactly n H characters in the file.
-  Prototype: def minOperations(n)
-  Returns an integer
-  If n is impossible to achieve, return 0
-  """
-  if n <= 1:
-    return 0
-
-  for e in range(2, (int(n/2)+1)):
-    if n % e == 0:
-      return minOperations(int(n/e)) + e
-  return n
+    """
+    write a  method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file.
+    Prototype: def minOperations(n)
+    Returns an integer
+    If n is impossible to achieve, return 0
+    """
+    if n <= 1:
+        return 0
+    for e in range(2, (int(n/2)+1)):
+        if n % e == 0:
+            return minOperations(int(n/e)) + e
+    return n
