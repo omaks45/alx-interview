a0eb0e99ad8de9adda9c766096729cb460dddeb0
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-05-21 03:49:13 +0100

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,30 +17,39 @@ def validUTF8(data):
     :type: List[int]
     :rtype: bool
     """
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
-    #iterate each integer in the array
-    for i in data:
-        # Get the binary representation. We only need the least significant 8 bits
-        # for any given number.
-        bin_form = format(i, '#010b')[-8:]
-	# process a new UTF-8 character.
-        if num_bytes == 0:
-            # Get the number of 1s in the beginning of the string.
-            for bit in bin_form:
-                if bit == '0': break
-                num_bytes += 1
+
+
+        # Number of bytes in the current UTF-8 character
+        n_bytes = 0
+
+        # Mask to check if the most significant bit (8th bit from the left) is set or not
+        mask1 = 1 << 7
+
+        # Mask to check if the second most significant bit is set or not
+        mask2 = 1 << 6
+        for num in data:
+
+            # Get the number of set most significant bits in the byte if
+            # this is the starting byte of an UTF-8 character.
+            mask = 1 << 7
+            if n_bytes == 0:
+                while mask & num:
+                    n_bytes += 1
+                    mask = mask >> 1
 
                 # 1 byte characters
-                if num_bytes == 0:
+                if n_bytes == 0:
                     continue
-                 # Invalid scenarios according to the rules of the problem.
-                if num_bytes == 1 or num_bytes > 4:
+
+                # Invalid scenarios according to the rules of the problem.
+                if n_bytes == 1 or n_bytes > 4:
                     return False
-        else:
-            # Else, we are processing integers which represent bytes which are a part of
-            # a UTF-8 character. So, they must adhere to the pattern `10xxxxxx`.
-            if not (bin_form[0] == '1' and bin_form[1] == '0'):
-                return False
-        num_bytes -= 1
-        return True
+            else:
+
+                # If this byte is a part of an existing UTF-8 character, then we
+                # simply have to look at the two most significant bits and we make
+                # use of the masks we defined before.
+                if not (num & mask1 and not (num & mask2)):
+                    return False
+            n_bytes -= 1
+            return n_bytes True
